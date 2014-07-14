0ccca7e4f072a6152c47aa8f94952bbfb0f7b169
FrantzLacarriere <frantz.lacarriere@gmail.com>
2014-07-14 10:27:55 -0400

    Hey guise added edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_devise_permitted_parameters
@@ -17,5 +18,10 @@ class ApplicationController < ActionController::Base
         |u| u.permit(registration_params)
       }
     end
+  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| 
+      u.permit(:password, :password_confirmation, :current_password) 
+    }
   end
 end
